5dca1aa408e5eabfb2bf594a7cb18e9af7a807a3
Karthik <theankitsingh2002@gmail.com>
2026-09-05 11:03:14 +0530

    navbar set

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
 
 
   return (
-    <Reveal>
+    
     <nav className="w-full px-4 py-5 sticky inset-0 z-50">
       <div className="max-w-4xl mx-auto flex justify-center ">
         <div className="flex items-center w-150 justify-center gap-8 sm:gap-12 border border-gray-700 bg-gray-600/10 backdrop-blur-2xl rounded-full px-10 sm:px-16 py-3 ">
@@ -55,7 +55,7 @@ const Navbar = () => {
         </div>
       </div>
     </nav>
-    </Reveal>
+    
   );
 };
 
